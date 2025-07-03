3044aa74cba34e313ad7a598d6f3918a039949d4
Sohard Begraj <140617987+Sohardbegraj@users.noreply.github.com>
2025-07-03 21:21:19 +0530

    Time: 1 ms (68.37%) | Memory: 9.1 MB (70.42%) - LeetSync

---
+++ 3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    char kthCharacter(int k) {
+        string res="a";
+        while(res.length()<k){
+            int n =res.length();
+            for(int i=0;i<n;i++){
+                char ch=res[i]=='z'?'a':res[i]+1;
+                res.push_back(ch);
+            }
+        }
+        return res[k-1];
+    }
+};
