ed0259c1ad706039acb45216e97d8d66b057793f
Sohard Begraj <140617987+Sohardbegraj@users.noreply.github.com>
2025-08-10 02:00:56 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (89.27%) - LeetSync

---
+++ 120-triangle/triangle.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n=triangle.size();
+        if(n==0){
+            return 0;
+        }
+        for (int row = n - 2; row >= 0; row--) {
+            for (int col = 0; col <= row; col++) {
+                triangle[row][col] += min(triangle[row+1][col], triangle[row+1][col+1]);
+            }
+        }
+        return triangle[0][0];
+    }
+
+};
