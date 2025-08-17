43a934eb0da629feff82ab7b88082ad0e5c7928c
Sohard Begraj <140617987+Sohardbegraj@users.noreply.github.com>
2025-08-17 23:30:31 +0530

    Time: 28 ms (5.98%) | Memory: 162.5 MB (5.33%) - LeetSync

--- 3954-maximum-balanced-shipments/maximum-balanced-shipments.cpp
+++ 3954-maximum-balanced-shipments/maximum-balanced-shipments.cpp
@@ -1,15 +1,26 @@
 class Solution {
 public:
     int maxBalancedShipments(vector<int>& weight) {
-        int n= weight.size();
-        if(n==0) return 0;
-        int count=0;
-        for(int i=1;i<n;i++){
-            if(weight[i-1]> weight[i]){
-                count++;
-                i++;
-            }
+    int n = weight.size();
+    vector<int> dp(n, 0);
+    vector<int> st;  // decreasing stack of indices
+
+    for (int i = 0; i < n; i++) {
+        dp[i] = (i > 0 ? dp[i-1] : 0);
+
+        while (!st.empty() && weight[st.back()] <= weight[i]) {
+            st.pop_back();
         }
-        return count;
+
+        if (!st.empty()) {
+            int j = st.back();
+            dp[i] = max(dp[i], (j > 0 ? dp[j-1] : 0) + 1);
+        }
+
+        st.push_back(i);
+    }
+
+    return dp[n-1];
+
     }
 };
