8537535d8f47a0ba52b852890f7be3b63676971f
Sohard Begraj <140617987+Sohardbegraj@users.noreply.github.com>
2025-08-26 00:26:42 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (21.07%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,21 +1,32 @@
 class Solution {
 public:
-    int solve(vector<int>& nums,int n,int i,vector<int>&memo){
-        if(i>=n)return 0;
-        if(memo[i]!=-1){
-            return memo[i];
+    int solve(vector<int>& nums, int start, int end) {
+        int n = end - start + 1;
+        if (n == 1) return nums[start];
+
+        vector<int> dp(n, 0);
+        dp[0] = nums[start];
+        dp[1] = max(nums[start], nums[start + 1]);
+
+        for (int i = 2; i < n; i++) {
+            int in = nums[start + i] + dp[i - 2];
+            int ex = dp[i - 1];
+            dp[i] = max(in, ex);
         }
-        int in=nums[i]+solve(nums,n,i+2,memo);
-        int ex=solve(nums,n,i+1,memo);
-        return memo[i]=max(in,ex);
+        return dp[n - 1];
     }
+
     int rob(vector<int>& nums) {
-        int n=nums.size();
-        if(n==1)return nums[0];
-        vector<int>memo1(n+1,-1);
-        int ex=solve(nums,n-1,0,memo1);// excluding last including first
-        vector<int>memo2(n+1,-1);
-        int in=solve(nums,n,1,memo2);// excluding first including last
-        return max(in,ex);
-    } 
+        int n = nums.size();
+        if (n == 0) return 0;
+        if (n == 1) return nums[0];
+
+        // Case 1: rob houses [0 … n-2] (exclude last)
+        int excludeLast = solve(nums, 0, n - 2);
+
+        // Case 2: rob houses [1 … n-1] (exclude first)
+        int excludeFirst = solve(nums, 1, n - 1);
+
+        return max(excludeLast, excludeFirst);
+    }
 };
