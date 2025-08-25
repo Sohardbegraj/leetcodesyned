8d9b63dd7d83ee7c95b4d5499a65e9ffb2da0b72
Sohard Begraj <140617987+Sohardbegraj@users.noreply.github.com>
2025-08-25 18:50:37 +0530

    Time: 29 ms (23.67%) | Memory: 31.2 MB (18.97%) - LeetSync

--- 498-diagonal-traverse/diagonal-traverse.cpp
+++ 498-diagonal-traverse/diagonal-traverse.cpp
@@ -4,19 +4,21 @@ public:
         int n=mat.size();
         int m=mat[0].size();
         vector<int>ans;
-        unordered_map<int,vector<int>>mp;
+        map<int,vector<int>>mp;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 mp[i+j].push_back(mat[i][j]);
             }
         }
-        for (int d = 0; d <= n + m - 2; d++) {
-            if (d % 2 == 0) {
-                reverse(mp[d].begin(), mp[d].end());
+        bool flip=true;
+        for (auto &it :mp) {
+            if (flip) {
+                reverse(it.second.begin(), it.second.end());
             }
-            for (int val : mp[d]) {
+            for (int val : it.second) {
                 ans.push_back(val);
             }
+            flip=!flip;
         }
 
         return ans;
