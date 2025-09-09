729f40fe96a9c6c92f586264b40f9e047a1e93cd
Sohard Begraj <140617987+Sohardbegraj@users.noreply.github.com>
2025-09-09 17:03:53 +0530

    Time: 1 ms (68.81%) | Memory: 9.3 MB (68.56%) - LeetSync

--- 2408-number-of-people-aware-of-a-secret/number-of-people-aware-of-a-secret.cpp
+++ 2408-number-of-people-aware-of-a-secret/number-of-people-aware-of-a-secret.cpp
@@ -1,31 +1,36 @@
 class Solution {
 public:
-    int M = 1e9+7;
+    int MOD = 1e9 + 7;
 
     int peopleAwareOfSecret(int n, int delay, int forget) {
-        vector<int> t(n+1);
+        vector<int> t(n + 1, 0);
         // t[day] = number of people who learn the secret on "day"
 
         t[1] = 1;
+        
+        int count = 0; 
 
-        for(int day = 2; day <= n; day++) {
-            long long count = 0;
-            for(int prev = day - forget+1; prev <= day - delay; prev++) {
-                if(prev > 0) {
-                    count = (count + t[prev]) % M;
-                }
+        for (int day = 2; day <= n; day++) {
+            // people who become eligible to share on this day
+            if (day - delay > 0) {
+                count = (count + t[day - delay]) % MOD;
             }
-            t[day] = count;
+            // people who forget on this day
+            if (day - forget > 0) {
+                count = (count - t[day - forget] + MOD) % MOD;
+            }
+            t[day] = count; //total number of people who will get to know today
         }
 
+
+        // Count the people who still remember on day n
         int result = 0;
-        for(int day = n-forget+1; day <= n; day++) {
-            if(day > 0) {
-                result = (result + t[day]) % M;
+        for (int day = n - forget + 1; day <= n; day++) {
+            if (day > 0) {
+                result = (result + t[day]) % MOD;
             }
         }
 
         return result;
-        
     }
 };
